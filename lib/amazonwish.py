3816fdfdb3f11664dfbfda42313ad6178e4b6972
Caio Begotti <caio1982@gmail.com>
2012-02-21 00:45:53 -0200

    adding the main module code, already able to retrieve the titles, their authors, prices, covers and universal tags (urls for external stores)

---
+++ lib/amazonwish.py
@@ -0,0 +1,37 @@
+from config import Config
+
+from lxml import etree
+
+class Scrapper():
+    def readConfig(self, country):
+        config = Config()
+        params = config.countryParams(country)
+        return params
+
+    def downloadPage(self, params, id):
+        domain = params['domain']
+        userid = id
+        url = 'http://www.amazon' + domain + '/wishlist/' + userid + '/ref=cm_wl_act_print_o?' + '_encoding=UTF8&layout=standard-print&disableNav=1&visitor-view=1&items-per-page=1000'
+        parser = etree.HTMLParser()
+        tree = etree.parse(url, parser)
+        return tree
+
+    def authors(self, page):
+        authors = page.xpath("/html/body/div[@id='printcfg']/div[@id='itemsTable']/div/form/table/tbody[*]/tr[1]/td[3]/div/span")
+        return authors
+    
+    def titles(self, page):
+        titles = page.xpath("/html/body/div[@id='printcfg']/div[@id='itemsTable']/div/form/table/tbody[*]/tr[*]/td[*]/div/strong")
+        return titles
+    
+    def prices(self, page):
+        prices = page.xpath("/html/body/div[@id='printcfg']/div[@id='itemsTable']/div/form/table/tbody[*]/tr[*]/td[@class='pPrice']/span/strong")
+        return prices
+    
+    def via(self, page):
+        via = page.xpath("/html/body/div[@id='printcfg']/div[@id='itemsTable']/div/form/table/tbody[*]/tr[*]/td[*]/strong[2]")
+        return via
+    
+    def covers(self, page):
+        covers = page.xpath("/html/body/div[@id='printcfg']/div[@id='itemsTable']/div/form/table/tbody[*]/tr[*]/td[*]/div[@class='pImage']/img/@src")
+        return covers
