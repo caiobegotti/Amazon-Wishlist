223f4c19028a2ad759ee6adc80c5cd7f84d85771
Caio Begotti <caio1982@gmail.com>
2012-02-21 15:02:36 -0200

    a simpler, yet longer, version of the test app showing all it can do so far (takes about 4s to return all data, in two full page retrievals)

--- basin.py
+++ basin.py
@@ -17,23 +17,41 @@ class Main:
         page = wl.downloadPage(params,'3MCYFXCFDH4FA')
 
         titles = wl.titles(page)
-        covers = wl.covers(page)
         authors = wl.authors(page)
+        covers = wl.covers(page)
         prices = wl.prices(page)
         via = wl.via(page)
 
+        print 'Your titles are:'
         for entry in titles: 
-            res = tostring(entry, encoding='utf-8', method='text', pretty_print=True).strip()
-            print res
+            print '\t' + entry
+
+        print 'The titles authors are:'
+        for entry in authors: 
+            print '\t' + entry
+
+        print 'Your items covers:'
+        for entry in covers: 
+            print '\t' + entry
+
+        print 'Their prices:'
+        for entry in prices: 
+            print '\t' + entry
+
+        print 'Some external sources:'
+        for entry in via: 
+            print '\t' + entry
 
         p = Profile()
         params = p.readConfig(country)
         page = p.downloadPage(params,'3MCYFXCFDH4FA')
 
         info = p.basicInfo(page)
+        print 'Your name and avatar:'
         print info
 
         lists = p.wishlistsDetails(page)
+        print 'Your lists and their sizes:'
         print lists
 
 if __name__ == "__main__":
