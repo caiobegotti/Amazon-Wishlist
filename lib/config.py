2d73798a92633d1f66e0c5a41aa2de8ecd7b2bb7
Caio Begotti <caio1982@gmail.com>
2012-02-21 01:00:55 -0200

    adds support for more worldwide stores, everybody now using unicode code points for currencies symbols

--- lib/config.py
+++ lib/config.py
@@ -4,7 +4,7 @@ class Config:
             config = {
                       'currency': 'USD',
                       'domain'  : '.com',
-                      'symbol'  : ur'$',
+                      'symbol'  : ur'\u0024',
                       } 
         elif 'uk' in country:
             config = {
@@ -16,8 +16,44 @@ class Config:
             config = {
                       'currency': 'CAD',
                       'domain'  : '.ca',
-                      'symbol'  : ur'$'
+                      'symbol'  : ur'\u0024'
                       }
+        elif 'fr' in country:
+            config = {
+                      'currency': 'EUR',
+                      'domain'  : '.fr',
+                      'symbol'  : ur'\u20ac',
+                     }
+        elif 'jp' in country:
+            config = {
+                      'currency': 'JPY',
+                      'domain'  : '.jp',
+                      'symbol'  : ur'\u00a5',
+                     }
+        elif 'de' in country:
+            config = {
+                      'currency': 'EUR',
+                      'domain'  : '.de',
+                      'symbol'  : ur'\u20ac',
+                     }
+        elif 'it' in country:
+            config = {
+                      'currency': 'EUR',
+                      'domain'  : '.it',
+                      'symbol'  : ur'\u20ac',
+                     }
+        elif 'es' in country:
+            config = {
+                      'currency': 'EUR',
+                      'domain'  : '.es',
+                      'symbol'  : ur'\u20ac',
+                     }
+        elif 'cn' in country:
+            config = {
+                      'currency': 'CNY',
+                      'domain'  : '.cn',
+                      'symbol'  : ur'\uffe5',
+                     }
         else:
             exit('Missing country parameter (e.g. us, uk, ca etc)')
         return config
