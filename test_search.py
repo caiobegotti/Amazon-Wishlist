968595a301242ba4c1831a5cf8b17e613f2fb399
Caio Begotti <caio1982@gmail.com>
2012-07-30 23:19:12 -0300

    adding a simple search test... which i might expand later with searches in other sites, eventually

---
+++ test_search.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2012 - Caio Begotti <caio1982@gmail.com>
+# Distributed under the GPLv2, see the LICENSE file.
+
+from amazonwish.amazonwish import Search
+
+class TestSearchUnitedStates:
+    def test_search(self):
+        s = Search('caio1982@gmail.com', country='us')
+        matches = s.list()[0]
+        assert matches[0] == 'Caio Begotti'
+        assert matches[1] == '3MCYFXCFDH4FA'
