0702ffad0993067103032813498c755a914e1933
Caio Begotti <caio1982@gmail.com>
2013-10-01 12:10:19 +0545

    adds the indian store parameter to the sample codes

--- profile.py
+++ profile.py
@@ -12,7 +12,7 @@ from amazonwish.amazonwish import Profile
 def basin():
     parser = optparse.OptionParser("Usage: %prog [options]")
     parser.add_option("-i", "--id", dest="id", type="string", help="wishlist ID (i.e. 3MCYFXCFDH4FA)")
-    parser.add_option("-s", "--store", dest="store", type="string", help="store domain [us, uk, ca, fr, es, it, de, jp, cn, br]")
+    parser.add_option("-s", "--store", dest="store", type="string", help="store domain [us, uk, ca, fr, es, it, de, jp, cn, br, in]")
 
     (options, args) = parser.parse_args()
     if options.id is None:
