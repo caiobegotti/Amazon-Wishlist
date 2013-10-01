79f0482036f96cc374547f8e040b6c5e89fe4c27
Caio Begotti <caio1982@gmail.com>
2013-10-01 12:16:16 +0545

    first set of info about the module so i can upload it to pypi

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='amazon-wishlist',
+      version='0.1',
+      description='Python version of the old and buggy Perl module WWW::Amazon::Wishlist',
+      author='Caio Begotti',
+      author_email='caio1982@gmail.com',
+      url='https://github.com/caio1982/amazon-wishlist',
+      install_requires=['pytest', 'lxml', 'BeautifulSoup'],
+      long_description=read('README.markdown'),
+      py_modules=['amazonwish.amazonwish'],
+      license='GNU General Public License v2 (GPLv2)',
+      keywords='amazon wishlist xpath scrapping',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+        'Topic :: Internet'])
