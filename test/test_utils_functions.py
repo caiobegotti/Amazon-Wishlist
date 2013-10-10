bc85cdc30ea7947ef0a74063fe114ed7c4456f5d
Caio Begotti <caio1982@gmail.com>
2013-10-10 10:39:06 +0545

    i think it is more accurate, but not very sure right now

--- test/test_utils_functions.py
+++ test/test_utils_functions.py
@@ -4,7 +4,7 @@ URL = "^http://.*amazon.*/images/.*\..*$"
 WISHLIST = "^[0-9A-Z]{13}$"
 NAME = "^[\w ']{1,}"
 NUMBER = "^\d{1,}"
-PRICE = "(\d+[.,]\d+)+"
+PRICE = "^((\d{1,3}[.,])+\d{1,2})$"
 
 def is_match_for(pattern, string):
     regex = re.compile(pattern)
