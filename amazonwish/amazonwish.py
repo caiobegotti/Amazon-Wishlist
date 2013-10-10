6928cf885dfad0f8a4087584ad84a534604003cd
Caio Begotti <caio1982@gmail.com>
2013-10-10 09:15:45 +0545

    i like that aspect of it

--- amazonwish/amazonwish.py
+++ amazonwish/amazonwish.py
@@ -10,7 +10,7 @@ Amazon stores which have that feature (stores which are Kindle-only
 do not). It comes with two sample applications to search and list data. It's
 also quite useful if you want to know the total cost of your Amazon wishlist.
 
-It's written using LXML and XPaths for better readability. It supports the
+It's written using LXML and XPaths for better speed, readability. It supports the
 Amazon stores in the US, Canada, UK, France, Spain, Italy, Germany, Japan, China
 and India. Brazilian and Mexican stores also have built-in support though
 they are not live yet.
