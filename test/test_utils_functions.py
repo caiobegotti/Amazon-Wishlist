c222642021ac6fcbe523f81d34f5476f478309ce
Caio Begotti <caio1982@gmail.com>
2013-10-10 13:23:32 +0545

    http://tools.ietf.org/html/rfc3986#appendix-B for the domains match, and fixed empty strings in titles

--- test/test_utils_functions.py
+++ test/test_utils_functions.py
@@ -6,8 +6,8 @@ NAME = "^[\w ']{1,}"
 NUMBER = "^\d{1,}"
 PRICE = "^((\d{1,3}[.,])+\d{1,2})$"
 DP = "^http://www.amazon.*/dp/[0-9A-Z]{10,}$"
-VIA = "^(http://)?(www.)?[0-9a-z]+\..*/"
-TITLE = "^[\w\W\s]+$"
+VIA = "^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?"
+TITLE = "^[\w\W\s]+$|^$"
 
 def is_match_for(pattern, string):
     regex = re.compile(pattern)
