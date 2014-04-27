3be3e82464ddc14441a5fcc690d4aef1a9031ff8
Caio Begotti <caio1982@gmail.com>
2014-04-27 19:26:46 -0300

    the shortest wishlist codes are 12-chars long actually

--- test/test_utils_functions.py
+++ test/test_utils_functions.py
@@ -1,7 +1,7 @@
 import re
 
 URL = "^http://.*amazon.*/images/.*\.[A-Za-z]{3,5}$"
-WISHLIST = "^[0-9A-Z]{10,}$"
+WISHLIST = "^[0-9A-Z]{12,}$"
 NAME = "^[\w ']{1,}"
 NUMBER = "^\d{1,}"
 PRICE = "^((\d{1,3}[.,])+\d{1,2})$"
